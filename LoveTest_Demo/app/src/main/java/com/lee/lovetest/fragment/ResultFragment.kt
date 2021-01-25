7de2c609687d524d78ff8ed89c934d35b8b6aff5
YG_Desk <yg4236@ajou.ac.kr>
2021-01-25 19:58:38 +0900

    LoveTest_Demo 21/01/25

---
+++ LoveTest_Demo/app/src/main/java/com/lee/lovetest/fragment/ResultFragment.kt
@@ -0,0 +1,61 @@
+package com.lee.lovetest.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.lee.lovetest.R
+import kotlinx.android.synthetic.main.fragment_result.*
+
+
+class ResultFragment : Fragment() {
+
+    var option = -1
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        option = arguments?.getInt("index")?:-1 // 전체가 null이면 -1 반환
+        return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        btn_home.setOnClickListener{
+            navController.navigate(R.id.action_resultFragment_to_mainFragment)
+        }
+
+        setResult(option)
+    }
+    fun setResult(option: Int){
+        when(option){
+            1 -> {
+                tv_main.text="You are a QUITTER!"
+                tv_sub.text="You can let the person easily."
+            }
+            2 -> {
+                tv_main.text="You should focus on yourself."
+                tv_sub.text="You become really clingy to your ex."
+            }
+            3 -> {
+                tv_main.text="You should take if easy."
+                tv_sub.text="You can do crazy things no matter what it takes."
+            }
+            4 -> {
+                tv_main.text="You are pretty nature."
+                tv_sub.text="You can easily accept the break-up"
+            }
+        }
+
+
+    }
+}
